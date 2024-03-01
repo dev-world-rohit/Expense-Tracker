315251d38a982b9d9bd256ad7f3fbffb27c4ff88
Rohit Dewaliya <136791205+dev-world-rohit@users.noreply.github.com>
2024-03-01 23:30:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,9 @@ def add_expense():
     return jsonify({"message": "Expense Added!"}), 201
 
 
-if __name__ == "__main__":
-    with app.app_context():
+with app.app_context():
         db.create_all()
+# if __name__ == "__main__":
+    
 
-    app.run(debug=False)
+#     app.run(debug=False)
